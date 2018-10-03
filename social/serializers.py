c8907770378c6cead389cb6bf7f2e3649bb94c5b
bodqhrohro <bodqhrohro@yandex.ru>
2018-10-03 18:27:09 +0300

    Add id to serializers output

--- social/serializers.py
+++ social/serializers.py
@@ -6,10 +6,10 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('name', 'email', 'password', 'location', 'bio', 'avatar')
+        fields = ('id', 'name', 'email', 'password', 'location', 'bio', 'avatar')
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ('title', 'body', 'owner')
+        fields = ('id', 'title', 'body', 'owner')
