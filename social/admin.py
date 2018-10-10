692f1dc88f28c33059ef337a2aafb0bfd0bfd369
bodqhrohro <bodqhrohro@gmail.com>
2018-10-10 16:15:39 +0300

    Add models to admin site

--- social/admin.py
+++ social/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, User, Like
+
+admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Like)
