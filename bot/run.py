2e5dbb8b8aaac578c01302406b475da542d9c092
bodqhrohro <bodqhrohro@gmail.com>
2018-10-21 02:47:44 +0300

    Add authentication in the bot

--- bot/run.py
+++ bot/run.py
@@ -14,17 +14,19 @@ import requests
 
 ENDPOINT = ''
 
-users = []
-
 
 def check_result(response, success_message):
     if response.ok:
         print(success_message)
+        return response
     else:
         print(response.text)
+        return None
 
 
 def generate_users(count):
+    users = []
+
     for i in range(count):
         name = fake.name()
         email = fake.email()
@@ -48,6 +50,20 @@ def generate_users(count):
         })
         check_result(result, 'Created user ' + name)
 
+    return users
+
+
+def generate_posts(max_count, users):
+    for email, password in users:
+        result = requests.post(ENDPOINT + 'api-token-auth/', data={
+            'username': email,
+            'password': password,
+        })
+        result = check_result(result, 'Authenticated as ' + email)
+        if result:
+            token = result.json()['token']
+            print(token)
+
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -58,4 +74,5 @@ if not ENDPOINT.endswith('/'):
 with open(script_directory + '/config.json', 'r') as config_file:
     config = json.load(config_file)
 
-    generate_users(config['number_of_users'])
+    users = generate_users(config['number_of_users'])
+    posts = generate_posts(config['max_posts_per_user'], users)
